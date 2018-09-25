50ad697c054de880dfef2e6111f804a98bf39ebe
Ksenia <ksundelok@gmail.com>
2018-09-25 12:43:20 +0300

    Task 2.7 correct filter

--- src/main/java/com/es/phoneshop/web/DosFilter.java
+++ src/main/java/com/es/phoneshop/web/DosFilter.java
@@ -11,8 +11,9 @@ import java.util.Map;
 
 public class DosFilter implements Filter {
     private Map<String, Integer> requestCountMap = Collections.synchronizedMap(new HashMap<>());
+    private Map<String, Long> requestTimeMap = Collections.synchronizedMap(new HashMap<>());
 
-    private static final int INTERVAL = 3;
+    private static final int INTERVAL = 5;
     private static final int MAX_COUNT_OF_REQUEST = 20;
 
 
@@ -24,24 +25,30 @@ public class DosFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("Fil " + request.getRemoteAddr());
-        long startTime = System.currentTimeMillis();
         String address = request.getRemoteAddr();
         Integer count = requestCountMap.get(address);
         if (count == null) {
+            requestTimeMap.put(address, System.currentTimeMillis());
             count = 1;
         } else {
             count += 1;
         }
         requestCountMap.put(address, count);
 
-        if (count < MAX_COUNT_OF_REQUEST &&  System.currentTimeMillis() - INTERVAL > startTime) {
-            ((HttpServletResponse) response).sendError(429);
-
+        if (count > MAX_COUNT_OF_REQUEST) {
+            if (System.currentTimeMillis() - requestTimeMap.get(address) < INTERVAL) {
+                ((HttpServletResponse) response).sendError(429);
+            }
         } else {
-            count = 0;
-            startTime = System.currentTimeMillis();
-            filterChain.doFilter(request, response);
+            if (System.currentTimeMillis() - requestTimeMap.get(address) > INTERVAL) {
+                requestCountMap.put(address, null);
+                requestTimeMap.put(address, System.currentTimeMillis());
+                filterChain.doFilter(request, response);
+            } else {
+                filterChain.doFilter(request, response);
+            }
         }
+
     }
 
     @Override
